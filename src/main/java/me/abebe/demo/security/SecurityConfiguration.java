69c03ce1fffa5c97f19a6e86793dd8b647aa790f
abebeasnake <abebeasnake@gmail.com>
2018-03-05 10:36:36 -0500

    modification on SSUDS

--- src/main/java/me/abebe/demo/security/SecurityConfiguration.java
+++ src/main/java/me/abebe/demo/security/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import me.abebe.demo.repo.AppUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -91,10 +93,7 @@ public class SecurityConfiguration  extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
             throws Exception{
-        auth.inMemoryAuthentication().
-                withUser("user").password("password").authorities("USER").
-                and().
-                withUser("dave").password("begreat").authorities("ADMIN");
+
 
 //        Database Authentication must come after in memory authentication
         auth
